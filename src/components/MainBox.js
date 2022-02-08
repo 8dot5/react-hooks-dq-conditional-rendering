5c2ce6f473fd3193cdd875d635fa7bea9ebda79d
Chun Yi <8.5by11@gmail.com>
2022-02-08 10:46:02 -0500

    code

--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -1,23 +1,64 @@
-import React from "react";
+// import React from "react";
+// import MenuBar from "./MenuBar";
+// import { Profile, Photos, Cocktails, Pokemon } from "./pages";
+
+// function MainBox() {
+//   /*
+//     Replace the code below! Depending on what menu item is selected in the menu, 
+//     I should render either a Profile, Photos, Cocktails, or Pokemon component.
+//     Think of a way to track which menu item was selected. 
+//     - Which component should have state? 
+//     - Which component should have methods to control state? 
+//     - Where should these methods be called?
+//   */
+
+//   let detailsToDisplay = <div>Hi, I'm a div!</div>;
+
+//   return (
+//     <div>
+//       <MenuBar />
+//       {detailsToDisplay}
+//     </div>
+//   );
+// }
+
+// export default MainBox;
+
+
+import React, { useState } from "react";
 import MenuBar from "./MenuBar";
 import { Profile, Photos, Cocktails, Pokemon } from "./pages";
 
 function MainBox() {
-  /*
-    Replace the code below! Depending on what menu item is selected in the menu, 
-    I should render either a Profile, Photos, Cocktails, or Pokemon component.
-    Think of a way to track which menu item was selected. 
-    - Which component should have state? 
-    - Which component should have methods to control state? 
-    - Where should these methods be called?
-  */
+  const [selectedItem, setSelectedItem] = useState("profile");
 
-  let detailsToDisplay = <div>Hi, I'm a div!</div>;
+  function handleMenuClick(item) {
+    setSelectedItem(item);
+  }
+
+  // let detailsToDisplay = () => {
+  //   if (selectedItem === "profile") {
+  //     return <Profile />;
+  //   } else if (selectedItem === "photos") {
+  //     return <Photos />;
+  //   } else if (selectedItem === "cocktails") {
+  //     return <Cocktails />;
+  //   } else {
+  //     return <Pokemon />;
+  //   }
+  // };
+
+  let detailsToDisplay = {
+    profile: <Profile />,
+    photos: <Photos />,
+    cocktails: <Cocktails />,
+    pokemon: <Pokemon />,
+  };
 
   return (
     <div>
-      <MenuBar />
-      {detailsToDisplay}
+      <MenuBar handleClick={handleMenuClick} selectedItem={selectedItem} />
+      {detailsToDisplay[selectedItem]}
     </div>
   );
 }
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,32 +1,75 @@
+// import React from "react";
+
+// function MenuBar(props) {
+//   /*
+
+//   The 'span' tags below are the menu items. Think about the way a menu 
+//   should work. When you click a menu item, the button typically becomes
+//   'active' to indicate that it is currently selected. How could we achieve
+//   this programatically? What other behavior do we expect when we click
+//   on a menu item? Do we need state in this component, and if not, how can
+//   this component be made aware of what is currently the active menu item?
+
+//   */
+
+//   return (
+//     <div className="ui four item menu">
+//       <span className="item active">
+//         <i className="user large icon" />
+//       </span>
+
+//       <span className="item">
+//         <i className="photo large icon" />
+//       </span>
+
+//       <span className="item">
+//         <i className="cocktail large icon" />
+//       </span>
+
+//       <span className="item">
+//         <i className=" themeisle large icon" />
+//       </span>
+//     </div>
+//   );
+// }
+
+// export default MenuBar;
+
+
 import React from "react";
 
-function MenuBar(props) {
-  /*
-
-  The 'span' tags below are the menu items. Think about the way a menu 
-  should work. When you click a menu item, the button typically becomes
-  'active' to indicate that it is currently selected. How could we achieve
-  this programatically? What other behavior do we expect when we click
-  on a menu item? Do we need state in this component, and if not, how can
-  this component be made aware of what is currently the active menu item?
-
-  */
+function MenuBar({ handleClick, selectedItem }) {
+  function setClassName(name) {
+    return selectedItem === name ? "item active" : "item";
+  }
 
   return (
     <div className="ui four item menu">
-      <span className="item active">
+      <span
+        onClick={() => handleClick("profile")}
+        className={setClassName("profile")}
+      >
         <i className="user large icon" />
       </span>
 
-      <span className="item">
+      <span
+        onClick={() => handleClick("photos")}
+        className={setClassName("photos")}
+      >
         <i className="photo large icon" />
       </span>
 
-      <span className="item">
+      <span
+        onClick={() => handleClick("cocktails")}
+        className={setClassName("cocktails")}
+      >
         <i className="cocktail large icon" />
       </span>
 
-      <span className="item">
+      <span
+        onClick={() => handleClick("pokemon")}
+        className={setClassName("pokemon")}
+      >
         <i className=" themeisle large icon" />
       </span>
     </div>
